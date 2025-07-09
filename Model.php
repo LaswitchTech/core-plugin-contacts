f43d25c24c10eef3384a21443471f89af7f24c5f
Louis Ouellet <louis_ouellet@hotmail.com>
2025-07-09 15:38:02 -0400

    General: Code Standardization & Clean-up

--- Model.php
+++ Model.php
@@ -1,165 +1,109 @@
 <?php
 
-/**
- * Core Framework - ContactsModel
- *
- * @license    MIT (https://mit-license.org/)
- * @author     Louis Ouellet <louis@laswitchtech.com>
- */
-
 // Import additionnal class into the global namespace
-use \LaswitchTech\Core\Abstracts\Model;
+use \LaswitchTech\Core\Base\BaseModel;
 
-class ContactsModel extends Model {
+class ContactsModel extends BaseModel {
 
     /**
-     * Create a new contact and return the id
-     *
-     * @param array $data
-     * @return int
+     * Constructor
      */
-    public function create(array $data): int
+    public function __construct()
     {
-        // Create the Query
-        $Query = $this->Database->query()
-            ->table('contacts')
-            ->insert($data);
+        // Call the parent constructor
+        parent::__construct();
 
-        // Execute the Query
-        $affectedRows = $Query->execute();
-
-        // Execute the Query
-        return $Query->lastId();
+        // Initialize the Model
+        $this->init('contacts');
     }
 
     /**
-     * Update a contact
+     * Retrieve multiple records
      *
-     * @param int $id
-     * @param array $data
-     * @return int
-     */
-    public function update(int $id, array $data): int
-    {
-        // Create the Query
-        $Query = $this->Database->query()
-            ->table('contacts')
-            ->update($data)
-            ->where('id', $id);
-
-        // Execute the Query
-        return $Query->execute();
-    }
-
-    /**
-     * Retrieve Contacts List
-     *
-     * @param string $table
-     * @param int $id
+     * @param array $conditions
      * @return array
      */
-    public function list(string $table, int $id): array
+    public function fetchAll(array $conditions = [], string $conjunction = 'AND'): array
     {
         // Create the Query
         $Query = $this->Database->query()
-            ->table($table)
+            ->table($this->table)
             ->select('*')
             ->join('owner', 'users', 'username')
             ->join('vcard', 'vcards', 'id')
             ->join('organization', 'organizations', 'id')
             ->filter()
             ->where('id', 9999, '<>')
-            ->where('id', $id)
-            ->where('isArchived', 1, '<>')
-            ->limit(1);
+            ->where('organization', $this->Auth->user()->organization()->id);
 
-        // Retrieve the Results
-        $target = $Query->fetch();
+        // Check if the conditions are empty
+        if(!empty($conditions)){
 
-        // Check if the target exists
-        if(empty($target)){
-            return [];
-        } else {
-            $target = $target[array_key_first($target)];
-            $target['vcard']['tags'] = json_decode($target['vcard']['tags'] ?? '[]', true);
-            $target['vcard']['industries'] = json_decode($target['vcard']['industries'] ?? '[]', true);
-        }
+            // Add a Filter
+            $Query->filter();
 
-        // Create the Query
-        $Query = $this->Database->query()
-            ->table('contacts')
-            ->select('*')
-            ->join('owner', 'users', 'username')
-            ->join('vcard', 'vcards', 'id')
-            ->join('organization', 'organizations', 'id')
-            ->filter()
-            ->where('id', 9999, '<>')
-            ->where('isArchived', 1, '<>')
-            ->where('targetTable', $table)
-            ->where('targetId', $id);
+            // Add the Conditions
+            foreach($conditions as $key => $condition){
 
-        // Check if the target is a lead or a client
-        switch($table){
-            case 'leads':
-                $Query->filter('OR')->where('targetTable', 'clients')->where('targetId', $target['client']);
-                break;
-            case 'clients':
-                $Query->filter('OR')->where('targetTable', 'leads')->where('targetId', $target['lead']);
-                break;
+                // Check if the key exists in the definition
+                if(!array_key_exists($condition['key'], $this->definition)){
+
+                    // Remove the key from the data
+                    unset($conditions[$key]);
+                    continue;
+                }
+
+                // Add the condition to the Query
+                $Query->where($condition["key"], $condition["value"], $condition["operator"], $conjunction);
+            }
         }
 
         // Retrieve the Results
-        $results = $Query->fetch();
+        $records = $Query->fetch();
 
-        // Decode JSON Fields
-        foreach($results as $key => $record){
+        // Loop through the records to process them
+        foreach($records as $key => $record){
 
-            // Decode JSON Fields
-            $result[$key]['vcard']['tags'] = json_decode($record['vcard']['tags'] ?? '[]', true);
-            $result[$key]['vcard']['industries'] = json_decode($record['vcard']['industries'] ?? '[]', true);
+            // Overwrite the record with the processed one
+            $records[$key] = $this->process($record);
         }
-
-        // Append the target to the results
-        $results[] = $target;
 
         // Return the Results
-        return $results;
+        return $records;
     }
 
     /**
-     * Retrieve Contact's Details
+     * Retrieve a single record
      *
      * @param int $id
      * @return array
      */
-    public function get(int $id): array
+    public function fetch(int $id): array
     {
         // Create the Query
         $Query = $this->Database->query()
-            ->table('contacts')
+            ->table($this->table)
             ->select('*')
             ->join('owner', 'users', 'username')
             ->join('vcard', 'vcards', 'id')
             ->join('organization', 'organizations', 'id')
             ->filter()
             ->where('id', 9999, '<>')
-            ->where('isArchived', 0)
             ->filter()
-            ->where('id', $id)
+            ->where($this->primary, $id)
             ->limit(1);
 
-        // Retrieve the Results
-        $result = $Query->result();
+        // Retrieve the record
+        $records = $Query->fetch();
 
-        // Decode JSON Fields
-        foreach($result as $key => $record){
+        // Loop through the records to process them
+        foreach($records as $key => $record){
 
-            // Decode JSON Fields
-            $result[$key]['vcard']['tags'] = json_decode($record['vcard']['tags'] ?? '[]', true);
-            $result[$key]['vcard']['industries'] = json_decode($record['vcard']['industries'] ?? '[]', true);
+            // Overwrite the record with the processed one
+            $records[$key] = $this->process($record);
         }
 
-        // Return the Results
-        return $result[array_key_first($result)] ?? [];
+        // Return the record or an empty array if not found
+        return $records[array_key_first($records)] ?? [];
     }
 }
