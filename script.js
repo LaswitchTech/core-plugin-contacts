69bcbf3b7684cc5165f2d0ceca571ad9ce033e65
Louis Ouellet <louis_ouellet@hotmail.com>
2025-07-29 12:33:17 -0400

    General: Updated with the new modal theming.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const ContactModalCreate = function(list, fields = {}){
         },
         function(modal,component){
             const componentModal = component;
-            component.header.addClass('text-bg-success');
+            component.addClass('modal-success');
             component.footer.submit.addClass('btn-success').removeClass('btn-link').attr({
                 "style": "border-bottom-right-radius: var(--bs-modal-inner-border-radius) !important;border-bottom-left-radius: var(--bs-modal-inner-border-radius) !important;",
             }).text(builder.Locale.get('Create'));
@@ -137,7 +137,7 @@ const ContactModalArchive = function(contact, elementContact = null){
             const componentModal = component;
 
             // Style the modal
-            component.header.addClass('text-bg-dark');
+            component.addClass('modal-dark');
             component.footer.submit.addClass('btn-dark').removeClass('btn-link').attr({
                 "style": "border-bottom-right-radius: var(--bs-modal-inner-border-radius) !important;border-bottom-left-radius: var(--bs-modal-inner-border-radius) !important;",
             }).text(builder.Locale.get('Archive'));
