da3a1dfe55a9467e65eb1a76d62080f70d9e30ef
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-15 13:02:44 -0400

    General: Added support for interlinked objects.

--- script.js
+++ script.js
@@ -4,20 +4,76 @@ function process_function_hasContact(task, value, callback = null){
     // Loop through the contact list
     if(typeof task.target !== 'undefined'){
 
-        // Ajax Request
-        API.endpoint('/contacts/fetchAll').data({
-            conditions: [
-                {key: 'targetTable', operator: '=', value: task.targetTable},
-                {key: 'targetId', operator: '=', value: task.targetId},
-                {key: 'isArchived', operator: '<>', value: 1},
-            ]
-        }).execute(function(response){
-            for(const [id, contact] of Object.entries(response.records ?? {})){
-                if($.isArray(contact.vcard.role) && contact.vcard.role.includes(value)){
-                    if(typeof callback === "function"){
-                        callback(task, contact);
+        // Array of advance target tables
+        const advancedTables = ['clients','leads','importers'];
+        const advancedColumns = {'clients':'client','leads':'lead','importers':'importer'};
+
+        // Initialize Promises Array
+        const Promises = [];
+
+        // Create a Promise to fetch contacts based on the targetTable and targetId
+        Promises.push(new Promise((resolve, reject) => {
+
+            // Ajax Request
+            API.endpoint('/contacts/fetchAll').data({
+                conditions: [
+                    {key: 'targetTable', operator: '=', value: task.targetTable},
+                    {key: 'targetId', operator: '=', value: task.targetId},
+                    {key: 'isArchived', operator: '<>', value: 1},
+                ],
+            }).suppress().execute(function(response){
+                resolve(response.records ?? {});
+            }, function(){
+                resolve({});
+            });
+        }));
+
+        // Check if the target table is in array
+        if(advancedTables.includes(task.targetTable)){
+
+            // Loop through the advance target tables
+            for(const table of advancedTables){
+
+                // Get the column name for the advance target table
+                const column = advancedColumns[table];
+
+                // Skip if the table is the same as the task target table
+                if(table === task.targetTable) continue;
+
+                // Check if the target has the advance target table loaded
+                if(typeof task.target[column] === 'undefined' || task.target[column] === null || typeof task.target[column]?.id === 'undefined' || task.target[column]?.id === null){
+                    continue;
+                }
+
+                // Add a Promise to fetch contacts based on the advance targetTable and targetId
+                Promises.push(new Promise((resolve, reject) => {
+
+                    // Ajax Request
+                    API.endpoint('/contacts/fetchAll').data({
+                        conditions: [
+                            {key: 'targetTable', operator: '=', value: table},
+                            {key: 'targetId', operator: '=', value: task.target[column]?.id ?? null},
+                            {key: 'isArchived', operator: '<>', value: 1},
+                        ],
+                    }).suppress().execute(function(response){
+                        resolve(response.records ?? {});
+                    }, function(){
+                        resolve({});
+                    });
+                }));
+            }
+        }
+
+        // Execute all Promises and stop when one of them finds a matching contact
+        Promise.all(Promises).then(results => {
+            for(const records of results){
+                for(const [id, contact] of Object.entries(records)){
+                    if($.isArray(contact.vcard.role) && contact.vcard.role.includes(value)){
+                        if(typeof callback === "function"){
+                            callback(task, contact);
+                        }
+                        return;
                     }
-                    break;
                 }
             }
         });
